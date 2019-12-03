ce47db9ba0d3c21e1475d3bed95bb365dc153239
Ikreknin@bk.ru <Ikreknin@bk.ru>
2019-12-03 18:32:17 +0300

    Homework #5

---
+++ src/components/ChatsContainer.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import styles from '../styles/ChatsContainer.module.css';
+import { ChatItem } from './ChatItem.jsx';
+
+export class ChatsContainer extends React.Component{
+	constructor() {
+		super();
+
+		this.fillChats = this.fillChats.bind(this);
+
+        this.state = {
+            chats: this.fillChats(),
+            outerStyle: styles.container,
+        }
+
+	    document.addEventListener('NewChat', this._onNewChat.bind(this));
+	    document.addEventListener('Hide', this._onHide.bind(this));
+	    document.addEventListener('ShowChats', this._onShowChats.bind(this));
+	}
+
+    fillChats() {
+        const incomingChats = JSON.parse(localStorage.getItem('chats'));
+        let result = [];
+        if (incomingChats != null){
+            for (let i = 0; i < incomingChats.length; i += 1) {
+                let chat = {};
+                chat.key = incomingChats[i].key;
+                chat.name = incomingChats[i].name;
+                result.push(<ChatItem chat={ chat } />);
+            }
+        }
+        return result;
+    }
+
+    _onNewChat(event) {
+    	event.preventDefault();
+        const incomingChats = JSON.parse(localStorage.getItem('chats'));
+        let chat = {};
+        chat.key = incomingChats[incomingChats.length - 1].key;
+        chat.name = incomingChats[incomingChats.length - 1].name;
+        const data = JSON.parse(localStorage.getItem(incomingChats[incomingChats.length - 1].key));
+        chat.lastMessageTime = data.lastMessageTime;
+        chat.lastMessage = data.lastMessage;
+        
+        let chats = this.state.chats;
+        chats.push(<ChatItem chat={ chat } />);
+        this.setState({chats: chats})
+        //console.log("add");
+    }
+
+    _onHide() {
+    	let localState = this.state;
+        localState.outerStyle = styles.hide;
+        this.setState(localState); 
+    }
+
+    _onShowChats() {
+    	let localState = this.state;
+        localState.outerStyle = styles.container;
+        this.setState(localState);
+    }
+
+    render() {
+    	return (
+			<div className={ this.state.outerStyle }>
+				{ this.state.chats }
+    		</div>
+		);
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
-import React from 'react'
-import { render } from 'react-dom'
-import { Provider } from 'react-redux'
-import store from './store'
-import Routes from './routes'
-import './styles/globalStyles.css'
-import * as serviceWorker from './utils/serviceWorker'
+import React from 'react';
+import { render } from 'react-dom';
+import { CustomHeader } from './components/CustomHeader.jsx';
+import { ChatsContainer } from './components/ChatsContainer.jsx';
+import { AddButton } from './components/AddButton.jsx';
+import { MessagesContainer } from './components/MessagesContainer.jsx';
+//import { UserPage } from './components/UserPage.jsx';
+import './styles/globalStyles.css';
+import * as serviceWorker from './utils/serviceWorker';
 
 render(
-  <Provider store={store}>
-    <Routes />
-  </Provider>,
-  document.getElementById('root'),
-)
+	<div>
+		<CustomHeader />
+		<ChatsContainer />
+		<MessagesContainer />
+		<AddButton />
+	</div>,
+	document.getElementById('root'),
+);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister()
+serviceWorker.unregister();
