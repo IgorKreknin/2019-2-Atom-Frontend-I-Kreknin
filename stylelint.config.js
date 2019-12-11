9b4f7be6e0661c77727422e15b18fbe3f6305d7b
igor <ikreknin@bk.ru>
2019-12-11 22:39:57 +0300

    Homework#5 Final

--- stylelint.config.js
+++ stylelint.config.js
@@ -2,34 +2,46 @@
 // See: https://stylelint.io/user-guide/configuration/
 
 module.exports = {
-	extends: [
-		'@wemake-services/stylelint-config-scss',
-		'stylelint-config-css-modules',
-		'stylelint-a11y/recommended',
-	],
-	plugins: ['stylelint-no-unsupported-browser-features', 'stylelint-a11y'],
+  extends: ['@wemake-services/stylelint-config-scss', 'stylelint-config-css-modules', 'stylelint-a11y/recommended'],
+  plugins: ['stylelint-no-unsupported-browser-features', 'stylelint-a11y', 'stylelint-no-indistinguishable-colors'],
 
-	rules: {
-		// ignore special `var-` css variables for `:export`
-		'property-no-unknown': [
-			true,
-			{
-				ignoreProperties: ['/^var-/'],
-			},
-		],
+  rules: {
+    // ignore special `var-` css variables for `:export`
+    'property-no-unknown': [
+      true,
+      {
+        ignoreProperties: ['/^var-/'],
+      },
+    ],
 
-		// custom plugins to work with
-		'plugin/no-unsupported-browser-features': [
-			true,
-			{
-				severity: 'warning',
-				ignore: ['flexbox'],
-			},
-		],
+    // custom plugins to work with
+    'plugin/no-unsupported-browser-features': [
+      true,
+      {
+        ignore: [
+          'css-fixed',
+          'css-animation',
+          'flexbox',
+          'css-boxshadow',
+          'border-radius',
+          'transforms2d',
+          'viewport-units',
+          'calc',
+          'outline',
+          'word-break',
+        ],
+      },
+    ],
 
-		// a11y
-		'a11y/content-property-no-static-value': true,
+    // a11y
+    'a11y/content-property-no-static-value': true,
 
-		'scale-unlimited/declaration-strict-value': ['/color/', 'fill', 'stroke'],
-	},
-};
+    'scale-unlimited/declaration-strict-value': ['/color/', 'fill', 'stroke'],
+
+    'selector-type-no-unknown': [true, { ignoreTypes: ['forminput', 'message'] }],
+
+    'plugin/no-low-performance-animation-properties': [true, { ignoreProperties: ['box-shadow'] }],
+
+    'plugin/stylelint-no-indistinguishable-colors': false,
+  },
+}
