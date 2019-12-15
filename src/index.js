91832defac462928a0fd58ee04de333a7af41f0f
igor <ikreknin@bk.ru>
2019-12-16 01:03:14 +0300

    Homework 6

--- src/index.js
+++ src/index.js
@@ -1,19 +1,32 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom'
 import { CustomHeader } from './components/CustomHeader.jsx'
 import { ChatsContainer } from './components/ChatsContainer.jsx'
 import { AddButton } from './components/AddButton.jsx'
 import { MessagesContainer } from './components/MessagesContainer.jsx'
+import { UserPage } from './components/UserPage.jsx'
 import './styles/globalStyles.css'
 import * as serviceWorker from './utils/serviceWorker'
 
 render(
-  <div>
-    <CustomHeader />
-    <ChatsContainer />
-    <MessagesContainer />
-    <AddButton />
-  </div>,
+  <Router>
+    <div>
+      <CustomHeader />
+      <Switch>
+        <Route path="/user">
+          <UserPage />
+        </Route>
+        <Route path="/chat">
+          <MessagesContainer />
+        </Route>
+        <Route path="/">
+          <ChatsContainer />
+          <AddButton />
+        </Route>
+      </Switch>
+    </div>
+  </Router>,
   document.getElementById('root'),
 )
 // If you want your app to work offline and load faster, you can change
