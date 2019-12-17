07c16f1fb224cf0c7c2dfcbb413efbd56c5edf70
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-17 18:48:39 +0000

    Deploy IgorKreknin/2019-2-Atom-Frontend-I-Kreknin to github.com/IgorKreknin/2019-2-Atom-Frontend-I-Kreknin.git:gh-pages

---
+++ precache-manifest.0c2942ad104c474321a292617c5cc5d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc1cef9e4ab45ca3fcdf2a58ed9d064c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d0ca3abe60c0d3123d7",
+    "url": "/static/css/main.df6f57a2.chunk.css"
+  },
+  {
+    "revision": "48dd585b649e3ab1ef59",
+    "url": "/static/js/2.e5766f52.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.e5766f52.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6d0ca3abe60c0d3123d7",
+    "url": "/static/js/main.8df5b8df.chunk.js"
+  },
+  {
+    "revision": "fdbc5e018855f6b81a27",
+    "url": "/static/js/runtime-main.00ec08af.js"
+  }
+]);
